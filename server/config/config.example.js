6a5ca60b6cdd0a014fc774c69fc2a66c3d3d4fa5
cramerj1470 <john@Cramerec.com>
2023-03-12 13:34:12 -0400

    change gitignore

--- server/config/config.example.js
+++ server/config/config.example.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || "development";
 const config = {
 	development: {
 		port: process.env.PORT || 8090,
-		dbURL: "mongodb+srv://John27:Test123@cluster1.nymha.mongodb.net/Pixar1",
+		dbURL: "mongodb+srv://<username>:<password>@cluster1.nymha.mongodb.net/<database>",
 		authCookieName: "x-auth-token",
 	},
 };
